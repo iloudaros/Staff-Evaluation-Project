b57714ba9e7c0e5dc086ef432ce0183d5cbd1fff
Christina Kt <xriskt@gmail.com>
2021-01-27 23:11:44 +0200

    ΑΛΛΑΓΕΣ ΣΤΑ PROCEDURES

--- Stored Procedures.sql
+++ Stored Procedures.sql
@@ -4,13 +4,31 @@ DELIMITER $
 
 CREATE PROCEDURE seeevalresults (em_name VARCHAR(25),em_surname VARCHAR(35))
 BEGIN 
-DECLARE em_name VARCHAR(25);
-DECLARE em_surname VARCHAR(35);
 DECLARE em_username VARCHAR(12);
+DECLARE ev_name VARCHAR(25);
+DECLARE ev_username VARCHAR(25);
 
 SELECT name INTO em_name FROM user WHEN employee.username = user.username;
 SELECT surname INTO em_surname FROM user WHEN employee.username = user.username;
-SELECT COUNT job_id FROM requestevaluation WHERE 
+
+SELECT name INTO ev_name FROM user WHEN evaluator.username=user.username;
+SELECT surname INTO ev_surname FROM user WHEN evaluator.username=user.username;
+
+SET @nm_ex = SELECT EXISTS(em_name);
+SET @sr_ex = SELECT EXISTS(em_surname);
+
+IF(@nm_ex=1) AND (@st_ex=1) THEN
+	SELECT username INTO em_username FROM user WHERE name=em_name AND surname=em_surname;
+	SELECT * FROM requestevaluation WHERE em_username=empl_usrname;
+	IF((SELECT * FROM requestevaluation WHERE em_username=empl_usrname)=NULL) THEN
+		SELECT 'You have not requsted evaluation.'
+	END IF;
+	SELECT EViD,empl_usrname,job_id,grade,comments FROM evaluationresult WHERE em_username=empl_usrname;
+	SELECT --ONOMATEPWNYMO EVALUATOR
+ELSE IF((SELECT EViD,empl_usrname,job_id,grade,comments FROM evaluationresult WHERE em_username=empl_usrname)=NULL) THEN
+	SELECT 'Evaluation in progress';
+	SELECT empl_usrname,phase,grade FROM evaluation WHERE em_username=empl_usrname;
+END IF;
 
 DELIMITER ; 
 
@@ -26,7 +44,7 @@ DECLARE e_phase TINYINT;
 DECLARE p_grade TINYINT;
 
 DECLARE ids CURSOR FOR 
-	SELECT id, evaluator FROM job WHERE --KATI LEIPEI 
+	SELECT id, evaluator FROM job WHERE evaluation.empl_usrname=employee.username AND evaluation.ev_username=job.evaluator;
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET eot=1;
 OPEN ids;
 FETCH ids INTO id_job, id_evaluator;
@@ -49,8 +67,8 @@ IF(@g1=1) AND (@g2=1) AND (g3=1); THEN
 	UPDATE evaluationresult
 	SET grade = (@fg1+@fg2+@fg3)/3;
 ELSE
-	SELECT 'Your evaluation grading is still in progress.'
-END IF
+	SELECT 'Your evaluation grading is still in progress.';
+END IF;
 
 END$
 

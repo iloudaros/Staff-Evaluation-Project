d0d618794fc49361ff80c206c637a69256c55ba4
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-02-13 04:25:47 +0200

    Triggers ready

--- Triggers/company_info.sql
+++ Triggers/company_info.sql
@@ -1,8 +1,17 @@
 DELIMITER $
 
-CREATE TRIGGER company_info BEFORE INSERT ON company
+CREATE TRIGGER company_info AFTER UPDATE ON company
 FOR EACH ROW
 BEGIN
+
+	if (OLD.DOY != NEW.DOY OR OLD.AFM != NEW.AFM OR OLD.NAME != NEW.NAME) then
+	set NEW.DOY = OLD.DOY;
+	set NEW.AFM = OLD.AFM;
+	set NEW.NAME = OLD.NAME;
+
+	select 'Οι αλλαγές αγνοήθηκαν' as '';
+	end if;
+
 
 END$
 DELIMITER ;
--- Triggers/no_change_rights.sql
+++ Triggers/no_change_rights.sql
@@ -1,8 +1,12 @@
 DELIMITER $
 
-CREATE TRIGGER no_change_rights BEFORE INSERT ON company
+CREATE TRIGGER no_change_rights AFTER UPDATE ON user
 FOR EACH ROW
 BEGIN
+
+	if (OLD.username != NEW.username OR OLD.is_admin != NEW.is_admin) then
+	SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'Δεν μπορείς να πραγματοποιήσεις αυτή την ενέργεια, επικοινώνησε με τον διαχειριστή της βάσης.';
+
 
 END$
 DELIMITER ;

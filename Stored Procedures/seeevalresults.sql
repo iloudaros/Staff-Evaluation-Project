a5b3f4c0ee90c496464412ec4a6a4105a24cc822
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-02-13 23:43:16 +0200

    Corrections and Reporting

--- Stored Procedures/seeevalresults.sql
+++ Stored Procedures/seeevalresults.sql
@@ -1,4 +1,4 @@
-DELIMITER $ 
+DELIMITER $
 
 CREATE PROCEDURE seeevalresults (em_name VARCHAR(25),em_surname VARCHAR(35))
 BEGIN
@@ -6,28 +6,48 @@ DECLARE em_username VARCHAR(12);
 DECLARE ev_name VARCHAR(25);
 DECLARE ev_username VARCHAR(25);
 
-SELECT name INTO em_name FROM user WHEN employee.username = user.username;
-SELECT surname INTO em_surname FROM user WHEN employee.username = user.username;
 
-SELECT name INTO ev_name FROM user WHEN evaluator.username=user.username;
-SELECT surname INTO ev_surname FROM user WHEN evaluator.username=user.username;
+SELECT username INTO emp_username FROM user WHERE user.name =em_name AND user.username=em_surname;
 
-SET @nm_ex = SELECT EXISTS(em_name);
-SET @sr_ex = SELECT EXISTS(em_surname);
+IF(em_username != NULL) THEN
 
-IF(@nm_ex=1) AND (@st_ex=1) THEN
-	SELECT username INTO em_username FROM user WHERE name=em_name AND surname=em_surname;
-	SELECT * FROM requestevaluation WHERE em_username=empl_usrname;
-	IF((SELECT * FROM requestevaluation WHERE em_username=empl_usrname)=NULL) THEN
-		SELECT 'You have not requsted evaluation.'
+	IF( not exists(SELECT * FROM requestevaluation WHERE em_username=empl_usrname)) THEN
+
+		SELECT 'Άι μωρ@ αφού δεν έχεις κάνει καν αίτηση ακόμη.' as '';
+
+	ELSE
+
+		SELECT 'Οι αιτήσεις σας κυρι@ τέτοι@ μου:' as '';
+
+		SELECT * FROM requestevaluation WHERE empl_usrname=em_username;
+
+		SELECT 'Τα αποτελέσματα σας κυρι@ τέτοι@ μου:' as '';
+
+		SELECT evaluationresult.empl_usrname,evaluationresult.job_id,evaluationresult.grade,evaluationresult.comments,user.name,user.surname
+		FROM evaluationresult
+		WHERE empl_usrname=em_username
+		INNER JOIN user on EvID = username;
+
+		END IF;
+
+
+		IF( exists(select job,empl_usrname from evaluation where phase = 1 and job,empl_usrname not in
+			(select job from evaluation where phase=3) THEN
+
+			SELECT 'Αυτές οι αξιολογήσεις δεν έχουν ολοκληρωθεί ακόμη:' as '';
+
+			SELECT evaluation.empl_usrname,evaluation.job,evaluation.grade,evaluation.phase,user.name,user.surname FROM evaluationresult
+			WHERE empl_usrname=em_username AND phase !=3 AND job,empl_usrname not in
+			(select job from evaluation where phase=3)
+			INNER JOIN user on EvID = username;
+
+		END IF;
+
+
+
 	END IF;
-	SELECT EViD,empl_usrname,job_id,grade,comments FROM evaluationresult WHERE em_username=empl_usrname;
-	SELECT --ONOMATEPWNYMO EVALUATOR
-ELSE IF((SELECT EViD,empl_usrname,job_id,grade,comments FROM evaluationresult WHERE em_username=empl_usrname)=NULL) THEN
-	SELECT 'Evaluation in progress';
-	SELECT empl_usrname,phase,grade FROM evaluation WHERE em_username=empl_usrname;
-END IF;
 
+
+
+END$
 DELIMITER ;
-
---Βάλε και ένα END$
--- Triggers/no_change_rights.sql
+++ Triggers/no_change_rights.sql
@@ -3,10 +3,11 @@ DELIMITER $
 CREATE TRIGGER no_change_rights AFTER UPDATE ON user
 FOR EACH ROW
 BEGIN
-
+set @getuser = replace(user(),'@localhost','')
+if (@getuser not in (select username from user where is_admin=1) )then
 	if (OLD.username != NEW.username OR OLD.is_admin != NEW.is_admin) then
 	SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'Δεν μπορείς να πραγματοποιήσεις αυτή την ενέργεια, επικοινώνησε με τον διαχειριστή της βάσης.';
-
-
+	end if;
+	end if;
 END$
 DELIMITER ;
